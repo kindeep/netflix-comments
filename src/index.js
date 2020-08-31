beb935d3b1f875e547ae69445e299fdaa561db6d
kindeep <kindeepsingh@gmail.com>
2020-08-31 13:09:18 -0400

    Chat container on the right

--- src/index.js
+++ src/index.js
@@ -4,28 +4,51 @@ import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import Comments from "./comments";
 
-alert('yus');
+function waitForElm(selector) {
+  return new Promise((resolve) => {
+    if (document.querySelector(selector)) {
+      return resolve(document.querySelector(selector));
+    }
 
-const COMMENTS_EL_ID = "comments";
+    const observer = new MutationObserver((mutations) => {
+      if (document.querySelector(selector)) {
+        resolve(document.querySelector(selector));
+        observer.disconnect();
+      }
+    });
 
-const createCommentsEl = () => {
-  console.log(document.body);
-  const el = document.createElement("div");
-  el.id = COMMENTS_EL_ID;
-  document.body.appendChild(el);
-  return document.getElementById(COMMENTS_EL_ID);
-};
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  });
+}
 
-let commentsEl = document.getElementById(COMMENTS_EL_ID) ?? createCommentsEl();
+(async () => {
+  const COMMENTS_EL_ID = "comments";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Comments />
-  </React.StrictMode>,
-  commentsEl
-);
+  const wrapper = await waitForElm(".nf-kb-nav-wrapper");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  const createCommentsEl = () => {
+    console.log(document.body);
+    const el = document.createElement("div");
+    el.id = COMMENTS_EL_ID;
+    wrapper.appendChild(el);
+    return document.getElementById(COMMENTS_EL_ID);
+  };
+
+  let commentsEl =
+    document.getElementById(COMMENTS_EL_ID) ?? createCommentsEl();
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <Comments />
+    </React.StrictMode>,
+    commentsEl
+  );
+
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://bit.ly/CRA-PWA
+  serviceWorker.unregister();
+})();
